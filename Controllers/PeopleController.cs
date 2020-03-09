f80c22ec97986fffb8f555c97f7a584808df1661
Gerald Maboshe <gerald@thezig.io>
2020-03-09 14:29:54 +0200

    Add project files.

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,49 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Demo.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        List<Person> people = new List<Person>();
+
+        public PeopleController()
+        {
+            people.Add(new Person { Id = 1, FirstName = "Gerald", LastName = "Maboshe" });
+            people.Add(new Person { Id = 2, FirstName = "Bob", LastName = "Tim" });
+            people.Add(new Person { Id = 3, FirstName = "Mubita", LastName = "Kim" });
+        }
+        // GET: api/People
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET: api/People/5
+        public Person Get(int id)
+        {
+            return people.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/People
+        public void Post(Person val)
+        {
+            people.Add(val);
+        }
+
+        // PUT: api/People/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
